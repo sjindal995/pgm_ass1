b2853741b664d9f2a9bdeb8c348a36eadde6d9c8
Sahil Jindal <sjindal995@gmail.com>
2016-09-10 17:58:42 +0530

    adjacency list structure

--- bayes/q1.py
+++ bayes/q1.py
@@ -1,48 +1,25 @@
 import sys
-from random import randint
+import random
+adj_list = []
 
 def randomBN():
 	n = int(sys.argv[1])
 	k = int(sys.argv[2])
-	left = []
-	adj_list = [];
-	for i in range(0,n):
-		left.append(i);
-		adj_list.append([])
-	queue = []
-	while (len(left) > 0):
-		if(len(queue) > 0):
-			cur_element = queue[0]
-			queue = queue[1:]
-			u = randint(0,k)
-			for pick_num in range(0,u-1):
-				if(len(left) > 0):
-					child_num = randint(0,len(left)-1)
-					child = left[child_num]
-					adj_list[cur_element].append(child+1)
-					left.remove(child)
-					queue.append(child)
-				else:
-					break
+	comp_list = []
+	for i in range(1,n+1):
+		comp_list.append(i)
+	for i in range(1,n+1):
+		n_children = random.randint(0,k)
+		if(n_children > n-1):
+			child_indices = comp_list
+			child_indices.remove(i)
 		else:
-			queue.append(left[0])
-			left.remove(left[0])
+			child_indices = random.sample(range(1,i) + range(i+1, n+1),n_children)
+		adj_list.append(child_indices)
+		print str(i) + " : " +  str(n_children) +  " : " + str(child_indices)
 
 	with open('random-bn.txt','w') as f:
 		for index in range(1,n+1):
 			f.write(str(index) + " " + str(adj_list[index-1]) + "\n")
-
-
-def randomBN():
-	n = int(sys.argv[1])
-	k = int(sys.argv[2])
-	adj_list = [];
-	for i in range(0,n):
-		adj_list.append([])
-		n_children = randint(0,k)
-		child_indices = random.sample(range(1,n),n_children)
-		with open('random-bn.txt','w') as f:
-			for index in range(1,n+1):
-				f.write(str(index) + " " + str(adj_list[index-1]) + "\n")
 
 randomBN()
