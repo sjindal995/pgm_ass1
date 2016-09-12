d3bcef6fc3dbd8eeca65cbc1c1bd69d848ce0e57
Sahil Jindal <sjindal995@gmail.com>
2016-09-12 10:20:21 +0530

    1st ques. complete

---
+++ OCRdataset/q2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+vector<char> chars = {'e','t','a','o','i','n','s','h','r','d'}
+double ocr_factor[10][1000];
+double transition_factor[10];
+
+double getConfProb(string line, bool include_transition, bool include_skip){
+	
+}
+
+int main(){
+
+}
